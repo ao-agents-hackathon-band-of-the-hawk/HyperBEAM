915d55ffebdae3edbcb11b13fe295e286ca70649
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2025-08-31 09:18:57 +0000

    update setup.sh from test/text-to-speech

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 
 sudo apt-get update && sudo apt-get install curl -y --no-install-recommends
 
-curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-cuda-keyring.gpg
+curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub | sudo gpg --dearmor --yes -o /usr/share/keyrings/nvidia-cuda-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/nvidia-cuda-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" | sudo tee /etc/apt/sources.list.d/cuda-repository.list
+
+# for python 3.12
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 
 sudo apt-get update && sudo apt-get install -y --no-install-recommends \
     build-essential \
@@ -16,26 +19,29 @@ sudo apt-get update && sudo apt-get install -y --no-install-recommends \
     sudo \
     curl \
     ca-certificates python3.12 python3.12-venv python3.12-dev libpython3.12-dev zsh zlib1g-dev cudnn9-cuda-12
-    
-#install uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
-#source uv
-source $HOME/.local/bin/env
 
 # Build and Install Erlang/OTP
-git clone --depth=1 --branch maint-27 https://github.com/erlang/otp.git && \
+if [ -z "$(erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' 2>/dev/null)" ]; then \
+    git clone --depth=1 --branch maint-27 https://github.com/erlang/otp.git && \
     cd otp && \
     ./configure --without-wx --without-debugger --without-observer --without-et && \
     make -j$(nproc) && \
     sudo make install && \
-    cd .. && rm -rf otp
+    cd .. && rm -rf otp; \
+else
+    echo "Erlang/OTP is already installed"
+fi
 
 # Build and Install Rebar3
-git clone --depth=1 https://github.com/erlang/rebar3.git && \
+if [ -z "$(rebar3 --version 2>/dev/null)" ]; then \
+    git clone --depth=1 https://github.com/erlang/rebar3.git && \
     cd rebar3 && \
     ./bootstrap && \
     sudo mv rebar3 /usr/local/bin/ && \
-    cd .. && rm -rf rebar3
+    cd .. && rm -rf rebar3; \
+else
+    echo "Rebar3 is already installed"
+fi
 
 # Install Rust and Cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
@@ -45,6 +51,12 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
 curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash - && \
     sudo apt-get install -y nodejs=22.16.0-1nodesource1 && \
     node -v && npm -v
+
+#install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+#source uv
+source $HOME/.local/bin/env
+uv --version
 
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libpython3.12.so  
 python3.12 -m venv .venv
